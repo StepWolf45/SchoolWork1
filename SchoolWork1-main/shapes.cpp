47dcc574d56cc119ffb5491e667a2324a7786ef5
Fr0kes <85014298+StepWolf45@users.noreply.github.com>
2022-09-26 22:12:44 +0300

    Update shapes.cpp

--- SchoolWork1-main/shapes.cpp
+++ SchoolWork1-main/shapes.cpp
@@ -20,17 +20,17 @@ int itc_spr(int a, int b) {
     }
 
 }
-int itc_str(int a, int b, int c) {
-    double p = (a + b + c) / 2;
-    if ((a + b > c && a + c > b && b + c > a) && (a > 0 && b > 0 && c > 0)) {
-        return itc_sqrt(p * (p - a) * (p - b) * (p - c));
 
-    }
-    else {
+int itc_str(int a, int b, int c){
+    if ((a < b + c && b < a + c && c < b + a) && (a > 0 && b > 0 && c > 0)){
+        double p = (a + b + c)/2;
+        return itc_sqrt(p * (p - a) * (p - b) * (p - c));
+    }else{
         return -1;
     }
 
 }
+
 double itc_scir(int radius) {
     double pi = 3.14;
     if (radius > 0) {
